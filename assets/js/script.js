23d89d75415b2a741b13d72f1a6efd7b4a39ec9b
FrancoVillalobos <villalobospintofranco@gmail.com>
2022-06-01 08:59:30 -0400

    se agrega al script smooth scroll, typeform y tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-/* Java de conexion con Typeform */
+// Smooth Scroll 
+$(document).ready(function () {
+    $("a").click(function (event) {
+        event.preventDefault();
+        var gato = this.hash;
+        $("html, body").animate({
+            scrollTop: $(gato).offset().top - 70,
+        }, 1000);
+    });
+});
+
+/* Typeform */
 $(document).ready(function () {
     var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement,
         gt = d.getElementsByTagName, id = "typef_orm",
@@ -8,7 +19,7 @@ $(document).ready(function () {
         }
 });
 
-/* inicializar el tooltip */
+/*tooltip */
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
